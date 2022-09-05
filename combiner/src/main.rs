9c22bcc85e13f389e1351658862fba5601fc8690
Lennon Garcia Dias Bravo <lennon.bravo@programmersinc.com>
2022-09-05 08:40:58 -0300

    modify: create FloatingImage struct

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,13 +1,31 @@
 mod args;
-use std::io::BufReader;
-use std::fs::File;
-use image::*;
 use args::Args;
-use image::io::Reader;
+use std::{io::BufReader, fs::File, convert::TryInto};
+use image::{io::Reader, DynamicImage, ImageFormat, imageops::FilterType::Triangle, GenericImageView };
 
 #[derive(Debug)]
 enum ImageDataErrors {
     DifferentImageFormats,
+}
+
+struct FloatingImage {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    name: String
+}
+
+impl FloatingImage {
+    fn new(width: u32, height: u32, name:String) -> Self {
+        let buffer_capacity = height * width * 4;
+        let buffer = Vec::with_capacity(buffer_capacity.try_into().unwrap());
+        FloatingImage {
+            width,
+            height,
+            data: buffer,
+            name
+        }
+    }
 }
 
 fn main() -> Result<(), ImageDataErrors> {
@@ -19,6 +37,8 @@ fn main() -> Result<(), ImageDataErrors> {
         return Err(ImageDataErrors::DifferentImageFormats);
     }
 
+    let (image_1, image_2) = stadardise_size(image_1, image_2);
+    let output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
     Ok(())
 }
 
@@ -27,4 +47,21 @@ fn find_image_path(path: String) -> (DynamicImage, ImageFormat) {
     let image_format: ImageFormat = image_reader.format().unwrap();
     let image: DynamicImage = image_reader.decode().unwrap();
     (image, image_format)
+}
+
+fn get_samllest_dimensions(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_2 = dim_2.0 * dim_2.1;
+    return if pix_1 < pix_2 {dim_1} else {dim_2};
+}
+
+fn stadardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, height) = get_samllest_dimensions(image_1.dimensions(), image_2.dimensions());
+    println!("widht: {}, height: {}\n", width, height);
+
+    if image_2.dimensions() == (width, height) {
+        (image_1.resize_exact(width, height, Triangle), image_2)
+    } else {
+        (image_2.resize_exact(width, height, Triangle), image_1)
+    }
 }
