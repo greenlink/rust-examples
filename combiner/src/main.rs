8173d8f2c6a49d75a15fb1f600ce221660e7769a
Greenlink <lennon@sistemasbr.net>
2022-09-06 22:59:22 -0300

    modify: finalizing combiner

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -6,6 +6,7 @@ use image::{io::Reader, DynamicImage, ImageFormat, imageops::FilterType::Triangl
 #[derive(Debug)]
 enum ImageDataErrors {
     DifferentImageFormats,
+    BufferTooSmall
 }
 
 struct FloatingImage {
@@ -23,8 +24,15 @@ impl FloatingImage {
             width,
             height,
             data: buffer,
-            name
+            name,
         }
+    }
+    fn set_data(&mut self, data: Vec<u8>) -> Result<(), ImageDataErrors> {
+        if data.len() > self.data.capacity() {
+            return Err(ImageDataErrors::BufferTooSmall);
+        }
+        self.data = data;
+        Ok(())
     }
 }
 
@@ -38,7 +46,11 @@ fn main() -> Result<(), ImageDataErrors> {
     }
 
     let (image_1, image_2) = stadardise_size(image_1, image_2);
-    let output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
+    let mut output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
+    let combine_data = combine_images(image_1, image_2);
+    output.set_data(combine_data)?;
+
+    image::save_buffer_with_format(output.name, &output.data, output.width, output.height, image::ColorType::Rgba8, image_format_1).unwrap();
     Ok(())
 }
 
@@ -52,7 +64,7 @@ fn find_image_path(path: String) -> (DynamicImage, ImageFormat) {
 fn get_samllest_dimensions(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
     let pix_1 = dim_1.0 * dim_1.1;
     let pix_2 = dim_2.0 * dim_2.1;
-    return if pix_1 < pix_2 {dim_1} else {dim_2};
+    return if pix_1 < pix_2 { dim_1 } else { dim_2 };
 }
 
 fn stadardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
@@ -62,6 +74,43 @@ fn stadardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImag
     if image_2.dimensions() == (width, height) {
         (image_1.resize_exact(width, height, Triangle), image_2)
     } else {
-        (image_2.resize_exact(width, height, Triangle), image_1)
+        (image_1, image_2.resize_exact(width, height, Triangle))
     }
+}
+
+fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
+    let vec_1 = image_1.to_rgba8().into_vec();
+    let vec_2 = image_2.to_rgba8().into_vec();
+
+    alternate_pixels(vec_1, vec_2)
+}
+
+fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+    let mut combined_data = vec![0u8; vec_1.len()];
+
+    let mut i = 0;
+    while i < vec_1.len() {
+        if i % 8 == 0 {
+            combined_data.splice(i..=i + 3, set_rgba(&vec_1, i, i+3));
+        } else {
+            combined_data.splice(i..=i + 3, set_rgba(&vec_2, i, i+3));
+        }
+        i += 4;
+    }
+
+    combined_data
+}
+
+fn set_rgba(vec: &Vec<u8>, start: usize, end: usize) -> Vec<u8> {
+    let mut rgba = Vec::new();
+
+    for i in start..=end {
+        let val: u8 = match vec.get(i) {
+            Some(d) => *d,
+            None => panic!("Index out of bounds")
+        };
+        rgba.push(val);
+    }
+
+    rgba
 }
