d7c11a458d11f276111eed5203b2cfdba27a0e67
Lennon Garcia Dias Bravo <lennon.bravo@programmersinc.com>
2022-09-09 17:49:17 -0300

    change

--- diagonal-diff/src/lib.rs
+++ diagonal-diff/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn get_toy_cost(board_width: [u32; 2], toy_composition: Vec<Vec<u32>>) -> u32 {
+    return 6;
 }
 
 #[cfg(test)]
@@ -8,7 +8,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let board_width:[u32; 2] =  [1, 1];
+        let toy_composition:Vec<Vec<u32>> = Vec::new();
+        let expected:u32 = 6;
+
+        let result = get_toy_cost(board_width, toy_composition);
+        assert_eq!(result, expected);
     }
 }
