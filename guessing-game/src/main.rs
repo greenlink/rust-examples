83702bb9ec10c8b4e88ec474ec7cd805b2b0cf12
Lennon Garcia Dias Bravo <lennon.bravo@programmersinc.com>
2022-09-20 11:37:30 -0300

    add: printing the guessed word.

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let mut guessed = print_guessed_word(guess);
+    println!("{}", guessed);
+
+}
+
+fn print_guessed_word(guessed_word: String) -> String {
+    let mut guessed_phrase = String::from("You guessed: ");
+    guessed_phrase.push_str(&guessed_word);
+    guessed_phrase
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_if_input_appears_on_final_result() {
+        let guessed_word = String::from("carnival");
+        let expect = String::from("You guessed: carnival");
+
+        let result = print_guessed_word(guessed_word);
+        assert_eq!(expect, result);
+    }
 }
