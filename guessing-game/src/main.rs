4bc8f477af5d79663433996097d83a22dceb1cc9
Lennon Garcia Dias Bravo <lennon.bravo@programmersinc.com>
2022-09-21 09:27:43 -0300

    modify: adding loop logic.

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,24 +1,47 @@
-use std::io;
+use std::{io, cmp::Ordering};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
+        let mut guess = String::new();
+
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("{}", print_guessed_word(guess));
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
+        println!("You guessed: {guess}");
+
+        let result = get_guess_result(secret_number, guess);
+
+        println!("{result}");
+        if result == String::from("You win!") {
+            break;
+        }
+    }
 }
 
-fn print_guessed_word(guessed_word: String) -> String {
-    let mut guessed_phrase = String::from("You guessed: ");
-    guessed_phrase.push_str(&guessed_word);
-    guessed_phrase
+
+fn get_guess_result(secret_number: u32, guess: u32) -> String {
+    let result:String;
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => result = String::from("Too small!"),
+        Ordering::Greater => result = String::from("Too big!"),
+        Ordering::Equal => result = String::from("You win!"),
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -26,11 +49,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_if_input_appears_on_final_result() {
-        let guessed_word = String::from("carnival");
-        let expect = String::from("You guessed: carnival");
+    fn test_if_guess_result_is_you_win() {
+        let expected = String::from("You win!");
+        let secret_number = 1;
+        let guess = 1;
 
-        let result = print_guessed_word(guessed_word);
-        assert_eq!(expect, result);
+        let result = get_guess_result(secret_number, guess);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_if_guess_result_is_too_big() {
+        let expected = String::from("Too big!");
+        let secret_number = 1;
+        let guess = 2;
+
+        let result = get_guess_result(secret_number, guess);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_if_guess_result_is_too_small() {
+        let expected = String::from("Too small!");
+        let secret_number = 2;
+        let guess = 1;
+
+        let result = get_guess_result(secret_number, guess);
+
+        assert_eq!(expected, result);
     }
 }
